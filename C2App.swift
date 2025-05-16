8624848cf2c2695a64edfcc4bc0d82c4de73d690
Win <iqbal.baihaqi911@gmail.com>
2025-05-16 08:55:49 +0800

    updating: The main home screen

---
+++ C2App.swift
@@ -0,0 +1,17 @@
+//
+//  C2App.swift
+//  C2
+//
+//  Created by Muhammad Iqbal Rizqi Baihaqi on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
